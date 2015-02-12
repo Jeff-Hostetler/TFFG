44e9a963947e2ab54702e2f3247f476be60eb50e
Jeff Hostetler <hostetjm@gmail.com>
2015-02-12 06:11:33 -0700

    prepped for deployment to heroku

--- config/environment.js
+++ config/environment.js
@@ -5,6 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'tffg',
     environment: environment,
     baseURL: '/',
+    adapterURL: process.env.ADAPTER_URL,
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
